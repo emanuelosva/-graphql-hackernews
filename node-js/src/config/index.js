09287d0f5243eea07ad27ec6229058ff9ee0ba87
emanuelosva <emanuelosva@gmail.com>
2020-09-14 13:22:14 -0500

    Add app settings

---
+++ node-js/src/config/index.js
@@ -0,0 +1,15 @@
+/**
+ * @fileoverview Settings
+ */
+
+require('dotenv').config()
+
+module.exports = {
+  app: {
+    dev: process.env.NODE_ENV !== 'production',
+    port: process.env.PORT | 3000,
+  },
+  auth: {
+    secret: process.env.SECRET || 'devSecret',
+  },
+}
