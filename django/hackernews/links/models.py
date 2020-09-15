06cd2b5c7117866a01d3db503a18c242aaba3e12
emanuelosva <emanuelosva@gmail.com>
2020-09-15 14:54:52 -0500

    Add Link model

--- django/hackernews/links/models.py
+++ django/hackernews/links/models.py
@@ -3,3 +3,13 @@ Links Models
 """
 
 from django.db import models
+
+
+class Link(models.Model):
+    """
+    Link model
+    """
+    url = models.URLField()
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
