ed3ce43f78a9e02256ec427ee43611b7e5ffb619
emanuelosva <emanuelosva@gmail.com>
2020-09-15 15:25:36 -0500

    Register gql url

--- django/hackernews/hackernews/urls.py
+++ django/hackernews/hackernews/urls.py
@@ -1,9 +1,15 @@
 """
 Hackernews URL Configuration
 """
+
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
 from django.contrib import admin
 from django.urls import path
 
+from .settings import PRODUCTION
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=not PRODUCTION)))
 ]
