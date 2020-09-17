e588ed060108ac09d2d3bd8264f2438cfb29b7e5
emanuelosva <emanuelosva@gmail.com>
2020-09-17 12:02:32 -0500

    Add personal info  query

--- django/hackernews/users/schema.py
+++ django/hackernews/users/schema.py
@@ -34,9 +34,17 @@ class Query(graphene.ObjectType):
     Users Querys.
     """
     users = graphene.List(UserType)
+    me = graphene.Field(UserType)
 
-    def resolve_users(self, info, **kwargs):
+    def resolve_users(self, info):
         return get_user_model().objects.all()
+
+    def resolve_me(self, info):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('Not Logged in!')
+
+        return user
 
 
 # *******************************
