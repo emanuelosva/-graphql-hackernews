8bc72747f5c5d20a101fb93db83158d07cc9d835
emanuelosva <emanuelosva@gmail.com>
2020-10-21 10:05:50 -0500

    Link types

--- node-code-first/src/schema/link/types.js
+++ node-code-first/src/schema/link/types.js
@@ -9,38 +9,45 @@ const { objectType } = require('@nexus/schema')
 module.exports.Link = objectType({
   name: 'Link',
   description: 'The linkt to some rosource shared to comunity',
-  definition (t) {
+  definition(t) {
+
     t.id('id', {
       nullable: false,
       description: 'Link pk'
     })
+
     t.string('description', {
       nullable: false,
       description: 'Link description'
     })
+
     t.string('url', {
       nullable: false,
       description: 'Url to the shared link'
     })
+
     t.string('createdAt', {
       nullable: true,
       description: 'Link creation date'
     })
+
     t.string('updatedAt', {
       nullable: true,
       description: 'Link update date'
     })
+
     t.field('postedBy', {
       type: 'User',
       nullable: true,
-      resolve (root, args, ctx) {
+      resolve(root, args, ctx) {
         return ctx.prisma.link.findOne({ where: { id: root.id } }).postedBy()
       }
     })
+
     t.list.field('votes', {
       type: 'Vote',
       nullable: true,
-      resolve (root, args, ctx) {
+      resolve(root, args, ctx) {
         return ctx.prisma.link.findOne({ where: { id: root.id } }).votes()
       }
     })
@@ -50,11 +57,13 @@ module.exports.Link = objectType({
 module.exports.Feed = objectType({
   name: 'Feed',
   description: 'Feed response info',
-  definition (t) {
+  definition(t) {
+
     t.int('count', {
       nullable: false,
       description: 'Link count'
     })
+
     t.list.field('links', {
       nullable: false,
       description: 'User links'
@@ -65,11 +74,13 @@ module.exports.Feed = objectType({
 module.exports.Vote = objectType({
   name: 'Vote',
   description: 'Represent a vote for some link',
-  definition (t) {
+  definition(t) {
+
     t.field('link', {
       nullable: false,
       description: 'The voted link'
     })
+
     t.field('user', {
       nullable: false,
       description: 'The user that emit a vote'
